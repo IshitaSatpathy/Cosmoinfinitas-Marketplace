f652a6fb2c2451cabfa52e7fc2ae423933d1382d
Biswa-bob <biswajeetpanda.bp@gmail.com>
2021-10-18 12:49:01 +0530

    added header error

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -4,9 +4,9 @@ const Header = () => {
   return (
     <>
       <main id="main">
-        {/* <section class="breadcrumbs">
-          <div class="container">
-            <div class="d-flex justify-content-between align-items-center">
+        {/* <section className="breadcrumbs">
+          <div className="container">
+            <div className="d-flex justify-content-between align-items-center">
               <h2>Inner Page</h2>
               <ol>
                 <li>
@@ -18,8 +18,8 @@ const Header = () => {
           </div>
         </section> */}
 
-        <section class="inner-page">
-          <div class="container"></div>
+        <section className="inner-page">
+          <div className="container"></div>
         </section>
       </main>
     </>
