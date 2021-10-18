8729a01f0e260eb34718cf91b9532b34901dc788
Abhisek1999 <56746981+Abhisek1999@users.noreply.github.com>
2021-10-18 14:21:31 +0530

    Deployed to different address

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftaddress = "0x4c0eaCDff18E7ce1C4A327c7362Bdf1FDc89463a";
-export const nftmarketAddress = "0xeAb5106d7E1E1fCF65b601F5EADe4367260afaaE";
+export const nftaddress = "0x283987C047B56F945237EE514d7317a98DFD5Fe0";
+export const nftmarketAddress = "0xd827d4CEC973c542f29f2412f48B06055BC551B9";
