619b98b9d9f98f8ab1599762c9886a930ecedfb9
Biswa-bob <biswajeetpanda.bp@gmail.com>
2021-10-26 23:59:05 +0530

    ETH

--- pages/index.js
+++ pages/index.js
@@ -134,7 +134,7 @@ export default function Home() {
                     >
                       <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
                     </svg>
-                    <span className="ml-1 lg:text-xl">{nft.price} Eth</span>
+                    <span className="ml-1 lg:text-xl">{nft.price} ETH</span>
                   </div>
                 </div>
                 <p className="mb-4 text-gray-500">{nft.description}</p>
