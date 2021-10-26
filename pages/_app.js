36c86fc9204f9ea288283953d2fec7fed798e43d
Biswa-bob <biswajeetpanda.bp@gmail.com>
2021-10-26 23:57:08 +0530

    error in css resolved

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
           href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;1,200&display=swap"
           rel="stylesheet"
