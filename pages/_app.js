29f33556cf2db7ab0b3eeb79f0c8029bd6506557
Biswa-bob <biswajeetpanda.bp@gmail.com>
2021-11-03 22:56:16 +0530

    added logo

--- pages/_app.js
+++ pages/_app.js
@@ -9,6 +9,27 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
@@ -51,7 +72,7 @@ function MyApp({ Component, pageProps }) {
         <div className="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
           <a href="index.html" className="logo me-auto me-lg-0">
             <img
-              src="assets/img/logo-main-nav.png"
+              src="assets/img/ci.png"
               alt="logo"
               width="55px"
               height="40px"
